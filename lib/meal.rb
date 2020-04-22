8a9edd822ea97fb0304d11abeebe4c475998dd8c
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-04-21 23:24:08 -0500

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,16 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
+  @@all = []
 
+  def initialize(waiter_obj, customer_obj, total, tip = 0)
+    @waiter = waiter_obj
+    @customer = customer_obj
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
