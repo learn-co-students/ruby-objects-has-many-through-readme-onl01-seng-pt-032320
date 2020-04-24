b1a2ae8b7b0783a60de045a4a2c561d60c006b36
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-24 17:57:24 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Customer
+  attr_accessor :name, :age
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter_obj, total_obj, tip=0)
+    Meal.new(waiter_obj, self, total_obj, tip)
+  end
+  
+  def meals
+    Meal.all.select{|m| m.customer == self}
+  end
+  
+  def waiters
+    meals.collect{ |m| m.waiter}
+  end
   
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Waiter
+attr_accessor :name, :years
+@@all = []
+
+def initialize(name, years_of_experience)
+  @name = name
+  @years = years_of_experience
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_meal(customer_obj,total,tip=0)
+  Meal.new(self, customer_obj, total, tip)
+end
+
+def meals
+  Meal.all.select{|m| m.waiter == self}
+end
+
+def best_tipper
+  best=0
+  customer=nil
+  meals.each do |m|
+    if best < m.tip
+      best = m.tip
+      customer = m.customer
+    end
+  end
+  customer
+end
 
 end
